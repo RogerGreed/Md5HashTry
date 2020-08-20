d6a48b50e5f40be84b805a398691a5255fdedfc5
Roger <temaman.89@gmail.com>
2020-08-20 13:24:00 +0300

    Добавьте файлы проекта.

---
+++ Md5HashTry/Md5Hash.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace Md5HashTryCs
+{
+    class Md5Hash
+    {
+         public static string ComputeMD5Checksum(string path)
+        {
+            using (FileStream fs = System.IO.File.OpenRead(path))
+            {
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] fileData = new byte[fs.Length];
+                fs.Read(fileData, 0, (int)fs.Length);
+                byte[] checkSum = md5.ComputeHash(fileData);
+                string result = BitConverter.ToString(checkSum).Replace("-", String.Empty);
+                Console.WriteLine(result);
+                return result;
+            }
+        }
+    }
+}
---
+++ Md5HashTry/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using Md5HashTryCs;
+
+
+namespace Md5HashTry
+
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.Write(args[i] + ":   ");
+                Md5Hash.ComputeMD5Checksum(args[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
